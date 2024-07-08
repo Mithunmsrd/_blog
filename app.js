048cf7d9954ddb52a729fef1efa4466af4d54683
Mithunmsrd <mithunanayadi@gmail.com>
2024-07-08 12:49:54 +0530

    hi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const mongoose=require('mongoose');
-const sample=require('./model/certi.js')
+const sample=require('./model/blog.js')
 const dotenv=require('dotenv');
 dotenv.config();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 
-const blogPosts = [];
+// const blogPosts = [];
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -48,22 +48,21 @@ app.get('/viewallblogs', (req, res) => {
 })
 
 app.get('/blog/:id', (req,res) => {
-    const id = req.params.id;
-    const blogs = blogPosts.find((blog) => blog.BlogID == id);
-    if (!blogs) {
-      return res.status(404).send("Blog not found");
-    }
+    // const id = req.params.id;
+    // const blogs = blogPosts.find((blog) => blog.BlogID == id);
+    // if (!blogs) {
+    //   return res.status(404).send("Blog not found");
+    // }
   
     res.sendFile(path.join(__dirname, 'public', 'viewblog.html'));
 })
 
-app.get('/api/blog/:id', (req,res) => {
-    const id = req.params.id;
-    const blogs = blogPosts.find(blog => blog.BlogID == id);
-    if (!blogs) {
-        return res.status(404).json({ error: 'Blog not found' });
-    }
-    res.json(blogs);
+app.get('/api/blog/:id', async (req,res) => {
+    const id= req.params.id;
+    const details= await sample.findOne({BlogID: id})
+    console.log(details);
+    res.json(details);
+    res.sendFile(path.join(__dirname, 'public', 'viewblog.html'));
 })
 
 app.post('/blog', async (req,res) => {
@@ -71,7 +70,7 @@ app.post('/blog', async (req,res) => {
         const data=req.body;
         console.log(data)
         const details= await sample.create(data);
-        res.status(201).redirect('/thank-you');
+        res.status(201).redirect('/');
     
        }
     catch(error){
@@ -79,6 +78,6 @@ app.post('/blog', async (req,res) => {
     }
 })
 
-app.listen(3000, () => {
+app.listen(3002, () => {
     console.log("The server is starting")
 })
